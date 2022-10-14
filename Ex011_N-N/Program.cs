78ebc8299c4e1b4f7e84b76f31139d152a17a35d
globa78 <pavlglob@gmail.com>
2022-10-15 00:10:16 +0300

    Ex011_N-N

---
+++ Ex011_N-N/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число:");
+int x = int.Parse(Console.ReadLine());
+int count = x;
+if (x<0)
+{
+    while (count < Math.Abs(x));
+{
+    Console.WriteLine(count);
+    count++;
+}
+}
+else
+{
+    count = - x;
+    while(count < x)
+{
+    Console.WriteLine(count);
+    count++;
+}
+}
+Console.WriteLine(x);
