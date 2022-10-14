85f76ba10ce32e41eb886f5e826330546b197724
globa78 <pavlglob@gmail.com>
2022-10-14 22:22:24 +0300

    Ex010_week

---
+++ Ex010_week/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+string x = Console.ReadLine();
+if (x == "1") Console.WriteLine("понедельник");
+if (x == "2") Console.WriteLine("вторник");
+if (x == "3") Console.WriteLine("среда");
+if (x == "4") Console.WriteLine("четверг");
+if (x == "5") Console.WriteLine("пятница");
+if (x == "6") Console.WriteLine("суббота");
+if (x == "7") Console.WriteLine("воскресенье");
