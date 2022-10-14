c96af83d64d7a3dbaa7e041db761861076495f41
globa78 <pavlglob@gmail.com>
2022-10-14 21:47:28 +0300

    Ex009_proverka_na_kvadrat

---
+++ Ex009_proverka_na_kvadrat/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число:");
+string input1 = Console.ReadLine();
+int x = int.Parse(input1);
+Console.WriteLine("Введите второе число:");
+string input2 = Console.ReadLine();
+int y = int.Parse(input2);
+if (x == y*y)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
