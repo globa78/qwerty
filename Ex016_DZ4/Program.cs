09e0e7795ee6aa4679babb70d92246d83379b5db
globa78 <pavlglob@gmail.com>
2022-10-15 17:58:03 +0300

    Ex016_DZ4

---
+++ Ex016_DZ4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число:");
+int x = int.Parse(Console.ReadLine());
+int count = x;
+count = 0;
+if (count<x && x % 2==0)
+{
+    while(count < x)
+{
+    count+=2;
+    Console.WriteLine(count);
+}
+}
+else 
+{
+    while(count < x)
+{
+    Console.WriteLine(count);
+    count+=2;
+}
+}
