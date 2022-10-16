1f972f25ac35777681c0523e32d3052bed9efa1f
globa78 <pavlglob@gmail.com>
2022-10-16 16:49:32 +0300

    Sem2_1-10-99

---
+++ Sem2_1-10-99/Program.cs
@@ -0,0 +1,17 @@
+﻿Random rnd = new Random();
+
+int  number = (int)rnd.NextInt64(10,99);
+Console.WriteLine(number);
+int firstNumber = 0;
+int secondNumber = 0;
+
+firstNumber = number/10;
+secondNumber = number % 10;
+if (firstNumber>secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(secondNumber);
+}
