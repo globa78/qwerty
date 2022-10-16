e330c9e53cdbc75a5d62fde53d230da3df96d762
globa78 <pavlglob@gmail.com>
2022-10-16 21:47:41 +0300

    Sem2_DZ2

---
+++ Sem2_DZ2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+string s = number.ToString();
+if ( number > 99)
+{
+    Console.WriteLine(s[2].ToString());
+}
+else 
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
