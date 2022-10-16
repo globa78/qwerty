97ac1fabff5cf9db024bbbf99371f62dcff458dd
globa78 <pavlglob@gmail.com>
2022-10-16 21:05:32 +0300

    Sem2_3

---
+++ Sem2_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число:");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int y = int.Parse(Console.ReadLine());
+int n = x%y;
+if (x % y == 0)
+{
+    Console.WriteLine("кратное");
+}
+else
+{
+    Console.WriteLine("не кратное, остаток:"+ n);
+}
+
