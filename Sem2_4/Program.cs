8b28fddb23777a0436725ee8537ddc1af41965e4
globa78 <pavlglob@gmail.com>
2022-10-16 21:16:14 +0300

    Sem2_4

---
+++ Sem2_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число:");
+int x = int.Parse(Console.ReadLine());
+
+if (x % 7 == 0 && x % 23 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
