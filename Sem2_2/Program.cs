fc0b19efdd85dc1b30ce7aa3cf177a2a5a4f86b7
globa78 <pavlglob@gmail.com>
2022-10-16 20:48:47 +0300

    Sem2_2

--- Sem2_2/Program.cs
+++ Sem2_2/Program.cs
@@ -1,10 +1,15 @@
-﻿Random rnd = new Random();
+﻿// Random rnd = new Random();
 
-int  number = (int)rnd.NextInt64(100,999);
-Console.WriteLine(number);
-int firstNumber = 0;
-int secondNumber = 0;
+// int  number = (int)rnd.NextInt64(100,999);
+// Console.WriteLine(number);
+// int firstNumber = 0;
+// int secondNumber = 0;
+// 
+// firstNumber = number/100;
+// secondNumber = number % 10;
+// Console.WriteLine(firstNumber*10 + secondNumber);
 
-firstNumber = number/100;
-secondNumber = number % 10;
-Console.WriteLine(firstNumber*10 + secondNumber);
+int number = new Random().Next(100,999);
+string s = number.ToString();
+Console.WriteLine(s);
+Console.WriteLine(s[0].ToString() + s[2].ToString());
