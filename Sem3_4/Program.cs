239fd3e5803800c6500fb00d08e489e8e297f254
globa78 <pavlglob@gmail.com>
2022-10-18 22:43:56 +0300

    Sem3_4

---
+++ Sem3_4/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("Введите N:");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    System.Console.Write($" {Math.Pow(i,2)} ");
+}
+
+/*int n = int.Parse(Console.ReadLine());
+double[] array = new double[n];
+
+for (int i = 0; i < n; i++)
+array[i] = Math.Pow(i + 1, 2);
+
+foreach (double i in array)
+Console.WriteLine(i)*/
