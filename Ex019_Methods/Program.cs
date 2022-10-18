6011a01126b5dffe2cfd5fabd5c488ca028fa305
globa78 <pavlglob@gmail.com>
2022-10-18 15:47:16 +0300

    Ex019_Methods

--- Ex019_Methods/Program.cs
+++ Ex019_Methods/Program.cs
@@ -77,3 +77,37 @@ for(int i = 2; i <= 10; i++)
     }
     Console.WriteLine();
 }
+
+// Работа с тектом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большгие "С" заменить маленькими "с".
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty";
+//             012345
+//s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result1 = String.Empty;
+    int length = text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result1 = result1 + $"{newValue}";
+        else result1 = result1 + $"{text[i]}";
+    }
+    return result1;
+}
+string newText = Replace(text, ' ', '|');
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+System.Console.WriteLine(newText);
+System.Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+System.Console.WriteLine(newText);
