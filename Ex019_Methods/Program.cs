43a18f444622dcdbf7514825f510abee4f2ef5b8
globa78 <pavlglob@gmail.com>
2022-10-18 16:04:26 +0300

    Ex019_Methods

--- Ex019_Methods/Program.cs
+++ Ex019_Methods/Program.cs
@@ -111,3 +111,40 @@ System.Console.WriteLine(newText);
 System.Console.WriteLine();
 newText = Replace(newText, 'С', 'с');
 System.Console.WriteLine(newText);
+
+
+// РАБОТА С МАССИВОМ
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array) 
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++) 
+    {
+        System.Console.Write($"{array[i]}");
+    }
+    System.Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    
+    for(int i = 0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if ( array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+
