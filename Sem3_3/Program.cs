694d57cd18e8ea2e71d3ea7f051b0d66e4d69fb8
globa78 <pavlglob@gmail.com>
2022-10-18 22:16:13 +0300

    Sem3_3

--- Sem3_3/Program.cs
+++ Sem3_3/Program.cs
@@ -21,3 +21,18 @@ array[1,1] = y2;
 double result = Math.Sqrt(Math.Pow((x1 - x2),2) + Math.Pow((y1 - y2),2));
 
 System.Console.WriteLine($"Длина ={result}");
+
+//System.Console.WriteLine("Ввод координат точки 1");
+//int x1 = int.Parse(Console.ReadLine());
+//int y1 = int.Parse(Console.ReadLine());
+//int [] array1 = new int[] {x1,y1};
+//System.Console.WriteLine("Ввод координат точки 2");
+//int x2 = int.Parse(Console.ReadLine());
+//int y2 = int.Parse(Console.ReadLine());
+//int [] array2 = new int[] {x2,y2};
+//
+//
+//
+//double result = Math.Sqrt(Math.Pow (array1[0] - array2[0],2) + Math.Pow (array1[1] - array2[1],2));
+//
+//System.Console.WriteLine($"Result is {result}");
