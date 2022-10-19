0899e0edc2ac8cc88abe1ba7a182f5fae28a4319
globa78 <pavlglob@gmail.com>
2022-10-19 09:25:06 +0300

    Sem4_SumN

---
+++ Sem4_SumN/Program.cs
@@ -0,0 +1,13 @@
+﻿int count = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine(GetSum(count));
+
+int GetSum(int count)
+{
+    int sum = 0;
+    for (int i = 0; i <= count; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
