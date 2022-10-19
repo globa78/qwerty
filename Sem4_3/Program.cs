24ca2cb58cccb9d7d21e8da21f80cfdd08e1b654
globa78 <pavlglob@gmail.com>
2022-10-19 10:33:53 +0300

    Sem4_3

---
+++ Sem4_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// System.Console.WriteLine("Введите число:");
+// int number = int.Parse(Console.ReadLine());
+// 
+// System.Console.WriteLine(GetF(number));
+// 
+// int GetF(int number)
+// {
+//     int F = 1;
+//     for (int i = 1; i <= number ; i++)
+//     {
+//         F = F*i;
+//     }
+//     return F;
+// }
+
+int number = int.Parse(Console.ReadLine());
+System.Console.WriteLine(GetF(number));
+
+int GetF(int number)
+{
+    if ( number <=1)
+    return 1;
+    return number * GetF(number - 1);
+}
