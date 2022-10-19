6c1cbe63b62ec1ddfec9a6db7f5aa38cc031df88
globa78 <pavlglob@gmail.com>
2022-10-19 19:48:21 +0300

    Sum4_DZ2

---
+++ Sum4_DZ2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 
+// 452 -> 11
+// 
+// 82 -> 10
+// 
+// 9012 -> 12
+
+// Console.Write("Введите число A: ");
+// int x = int.Parse(Console.ReadLine());
+// 
+// int SumNumber(int x)
+//   {
+//     
+//     int counter = Convert.ToString(x).Length;
+//     int advance = 0;
+//     int result = 0;
+// 
+//     for (int i = 0; i < counter; i++){
+//       advance = x - x % 10;
+//       result = result + (x - advance);
+//       x = x / 10;
+//     }
+//    return result;
+//   }
+// 
+// int sumNumber = SumNumber(x);
+// Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+Console.Write("Введите число A: ");
+int x = int.Parse(Console.ReadLine());
+int counter = Convert.ToString(x).Length;
+int advance = 0;
+int result = 0;
+
+for (int i = 0; i < counter; i++)
+{
+    advance = x - x % 10;
+    result = result + (x - advance);
+    x = x / 10;
+}  
+System.Console.WriteLine(result);
