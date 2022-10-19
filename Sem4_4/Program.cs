9429122a8c171af6d817b738eaf9fb0bb0d8637d
globa78 <pavlglob@gmail.com>
2022-10-19 10:58:14 +0300

    Sem4_4

---
+++ Sem4_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Massiv();
+void Massiv()
+{
+    int[] N = new int [8];
+    System.Console.Write("Массив:[");
+
+    for (int i = 0; i < N.Length; i++)
+    {
+        N[i] = new Random().Next(0,2);
+        System.Console.Write($"{N[i]} ");
+    }
+    System.Console.Write("]");
+}
