bbe5700dceec4818799cd46b2f918ef8b241ac64
globa78 <pavlglob@gmail.com>
2022-10-19 10:12:54 +0300

    Sem4_2

---
+++ Sem4_2/Program.cs
@@ -0,0 +1,29 @@
+﻿//System.Console.WriteLine("Введите число:");
+//int count = int.Parse(Console.ReadLine());
+//
+//System.Console.WriteLine(GetLength(count));
+//
+//int GetLength(int count)
+//{
+//    int sum = 0;
+//    while (true)
+//    {
+//        if (count == 0)
+//        break;
+//        count = count/10;
+//        sum++;
+//    }
+//    return sum;
+//}
+
+
+System.Console.WriteLine("Введите число:");
+string input = Console.ReadLine();
+int count = int.Parse(input);
+
+System.Console.WriteLine(GetLength(input));
+
+int GetLength(string input)
+{
+    return input.Length;
+}
