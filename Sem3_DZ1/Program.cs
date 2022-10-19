8f770a44e2c37d179f4260b4b8e3de66046e82c5
globa78 <pavlglob@gmail.com>
2022-10-19 09:01:45 +0300

    Sem3_DZ1

---
+++ Sem3_DZ1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 
+// 14212 -> нет
+// 
+// 12821 -> да
+// 
+// 23432 -> да
+System.Console.Write("Введите полиандр:");
+int number = int.Parse(Console.ReadLine());
+string s = number.ToString();
+if (s[0].ToString() == s[4].ToString() && s[1].ToString() == s[3].ToString())
+{
+    System.Console.WriteLine("да"); 
+}
+else
+{
+    System.Console.WriteLine("это не полиандр");
+}
