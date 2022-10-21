baccce9d98df98d06249e0a8c80cbc1fefcf5caf
globa78 <pavlglob@gmail.com>
2022-10-21 15:55:17 +0300

    Ex020_RecursionAlgorithm

---
+++ Ex020_RecursionAlgorithm/Program.cs
@@ -0,0 +1,122 @@
+﻿// string[,] table = new string[2,5];
+// table[1,2] = "+";
+// for (int stroki = 0; stroki < 2; stroki++ )
+// {
+//     for (int stolbcy = 0; stolbcy < 5; stolbcy++ )
+//     {
+//         System.Console.WriteLine($"-{table[stroki, stolbcy]}-");
+//     }
+// }
+
+
+// void PrintArray(int[,] matr) 
+// {
+// for (int i = 0; i < matr.GetLength(0); i++ )
+// {
+//     for (int j = 0; j < matr.GetLength(1); j++ )
+//     {
+//         System.Console.Write($"{matr[i,j]} ");
+//     }
+//     System.Console.WriteLine();
+// }
+// }
+// 
+// void FillArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++ )
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++ )
+//         {
+//             matr[i,j] = new Random().Next(1,10);
+//         }
+//     }
+// } 
+// 
+// int[,] matrix = new int[3,4];
+// PrintArray(matrix);
+// FillArray(matrix);
+// System.Console.WriteLine();
+// PrintArray(matrix);
+
+
+// int[,] pic = new int[,]
+// {
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+// {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// };
+// 
+// 
+// void PrintImage(int[,] image) 
+// {
+// for (int i = 0; i < image.GetLength(0); i++ )
+// {
+//     for (int j = 0; j < image.GetLength(1); j++ )
+//     {
+//         //System.Console.Write($"{image[i,j]} ");
+//         if (image[i,j] == 0)System.Console.Write($" ");
+//         else System.Console.Write($"+");
+//     }
+//     System.Console.WriteLine();
+//     }
+// }
+// 
+// void FillImage(int row, int col)
+// {
+//     if (pic[row,col]== 0)
+//     {
+//         pic[row,col] = 1;
+//         FillImage(row-1,col);
+//         FillImage(row,col-1);
+//         FillImage(row+1,col);
+//         FillImage(row,col+1);
+// 
+//     }
+// }
+// 
+// 
+// PrintImage(pic);
+// FillImage(13,13);
+// PrintImage(pic);
+
+// int FActorial(int n)
+// {
+//     if (n == 1 || n == 0) return 1;
+//     else return n*FActorial(n-1);
+// }
+// System.Console.WriteLine(FActorial(3));
+
+//f(1) = 1
+//f(2) = 1
+//f(n) = f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if (n==2 || n==1)return 1;
+    else return Fibonacci(n-1)+Fibonacci(n-2);
+}
+
+for (int i = 1; i<10;i++)
+{
+    System.Console.WriteLine(Fibonacci(i));
+}
