979047d282dad35b3e9484563c576ba6745d013f
globa78 <pavlglob@gmail.com>
2022-10-23 16:11:15 +0300

    Sem6_4

--- Sem6_4/Program.cs
+++ Sem6_4/Program.cs
@@ -2,7 +2,7 @@
 // Если N = 5 -> 0 1 1 2 
 // Если N = 3 -> 0 1 
 // Если N = 7 -> 0 1 1 2 3 5 8
-// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
 
 Console.WriteLine("Enter your number: ");
 int n = int.Parse(Console.ReadLine());
@@ -35,3 +35,39 @@ array [i] = array [i-1] + array [i-2];
 return array;
 
 }
+/*
+int[] CreateEmptyArrayForFibbonachi(int size)
+{
+int[] array = new int[size];
+
+array[0] = 0;
+array[1] = 1;
+
+return array;
+}
+
+string ArrayToString(int[] array)
+{
+string result = string.Empty;
+
+for (int i = 0; i < array.Length; i++)
+{
+result += array[i] + " ";
+}
+
+return result;
+}
+
+int[] GetFibonachi(int[] emptyArray)
+{
+for (int i = 2; i < emptyArray.Length; i++)
+{
+emptyArray[i] = emptyArray[i - 1] + emptyArray[i - 2];
+}
+
+return emptyArray;
+}
+
+int[] fib = CreateEmptyArrayForFibbonachi(8);
+fib = GetFibonachi(fib);
+Console.WriteLine(ArrayToString(fib));*/
