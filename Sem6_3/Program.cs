bf445dc8e3c04a00af4bad3f78654ee17efe58ba
globa78 <pavlglob@gmail.com>
2022-10-23 13:41:51 +0300

    Sem6_3

---
+++ Sem6_3/Program.cs
@@ -0,0 +1,15 @@
+﻿string DecNumber(int number)
+{
+string result = string.Empty;
+
+while (number > 0)
+{
+result += number % 2;
+number /= 2;
+Console.WriteLine(number);
+}
+
+return result;
+}
+
+Console.WriteLine(DecNumber(45));
