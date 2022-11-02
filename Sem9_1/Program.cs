af870492d10109c75a99b95131e262e580589997
globa78 <pavlglob@gmail.com>
2022-11-02 13:11:39 +0300

    Задача 63 и 65

---
+++ Sem9_1/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+//63
+/*System.Console.Write("Введите N ");
+int N = int.Parse(Console.ReadLine());
+
+void getNumbers(int N)
+{
+    if (N == 1)
+    {
+    System.Console.WriteLine(N);
+    return;
+    }
+    else
+    {
+    System.Console.WriteLine(N);
+    N-=1;
+    getNumbers(N);
+    }
+}
+getNumbers(N);*/
+//65
+System.Console.Write("Введите N ");
+int N = int.Parse(Console.ReadLine());
+System.Console.Write("Введите M ");
+int M = int.Parse(Console.ReadLine());
+
+void getNumbersNM(int N, int M)
+{
+    if (M>=N)
+    {
+        if (M == N)
+        {
+        System.Console.WriteLine(M);
+        return;
+        }
+        else
+        {
+        System.Console.WriteLine(M);
+        M-=1;
+        getNumbersNM(N,M);
+        }
+    }
+    else System.Console.WriteLine($"{N}>{M} введите правильные данные");
+}
+getNumbersNM(N,M);
