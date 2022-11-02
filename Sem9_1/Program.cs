35bb30ebd99054aae0e81360f791ca3c43904898
globa78 <pavlglob@gmail.com>
2022-11-02 13:22:16 +0300

    исправил

--- Sem9_1/Program.cs
+++ Sem9_1/Program.cs
@@ -50,3 +50,29 @@ void getNumbersNM(int N, int M)
     else System.Console.WriteLine($"{N}>{M} введите правильные данные");
 }
 getNumbersNM(N,M);
+
+//
+/*
+using System.Text;
+
+StringBuilder GetNumbers(int i, StringBuilder sb = null)
+{
+if (sb is null)
+{
+sb = new StringBuilder();
+}
+
+if (i == 1)
+{
+sb.Append(i.ToString() + " ");
+return sb;
+}
+
+GetNumbers(i - 1, sb);
+sb.Append(i.ToString() + " ");
+
+return sb;
+}
+
+Console.WriteLine(GetNumbers(5).ToString());
+*/
